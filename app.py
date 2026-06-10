f1f9461e28e41284a453abde57309fbbc768cce4
MatinMorady <matin80000@gmail.com>
2026-06-10 06:08:39 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, send_from_directory
 import os
+import uuid
 
 app = Flask(__name__)
 
@@ -9,22 +10,29 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
+# صفحه اصلی
 @app.route("/")
 def index():
     files = os.listdir(UPLOAD_FOLDER)
     return render_template("index.html", files=files)
 
 
+# آپلود امن‌تر
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
         file = request.files["file"]
+
         if file:
-            file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+            # جلوگیری از اسم تکراری
+            filename = str(uuid.uuid4()) + "_" + file.filename
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+
         return redirect("/")
     return render_template("upload.html")
 
 
+# دانلود
 @app.route("/download/<filename>")
 def download(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
