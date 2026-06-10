458f691eda798d3507dc92febdb775e71542a781
MatinMorady <matin80000@gmail.com>
2026-06-10 06:54:39 +0330

    Update models.py

--- models.py
+++ models.py
@@ -5,14 +5,14 @@ db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(200))
     is_admin = db.Column(db.Boolean, default=False)
 
 class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(200))
     title = db.Column(db.String(200))
+    filename = db.Column(db.String(200))
     views = db.Column(db.Integer, default=0)
 
 class Like(db.Model):
@@ -25,3 +25,13 @@ class Comment(db.Model):
     user_id = db.Column(db.Integer)
     song_id = db.Column(db.Integer)
     text = db.Column(db.String(300))
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+
+class PlaylistItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    playlist_id = db.Column(db.Integer)
+    song_id = db.Column(db.Integer)
